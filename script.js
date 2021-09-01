6cc4cd78256d347053929bb4209b2486fdcf6000
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-01 16:03:36 +0200

    Axios

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 console.log('Vue', Vue);
 
-const root = new Vue ({
-    el = "#root",
+const root = new Vue({
+    el: "#root",
     data: {
-
+        email: "",
     },
     methods: {},
+    created(){
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res)=>{
+            const { response } = res.data;
+            console.log(response);
+            this.email = response;
+        })
+    }
 })
