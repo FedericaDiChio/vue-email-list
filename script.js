4c1cdb449eeefc7365ff72f51c2ae945766e6328
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-02 13:32:31 +0200

    email sorted

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const root = new Vue({
         emails: [],
     },
     methods: {},
+    computed: {
+        sortedEmails(){
+            return this.emails.sort();
+        }
+    },
     created(){
         for(let i = 0; i < 10; i++){
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res)=>{
