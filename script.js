445094bc702ce33b81188526e8a7ff97fd51fff1
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-02 13:31:42 +0200

    ciclo for

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ console.log('Vue', Vue);
 const root = new Vue({
     el: "#root",
     data: {
-        email: "",
+        emails: [],
     },
     methods: {},
     created(){
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res)=>{
-            const { response } = res.data;
+        for(let i = 0; i < 10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res)=>{
+            const email = res.data.response;
             // console.log(response);
-            this.email = response;
-        })
-    }
+            this.emails.push(email);
+            })
+        }    
+    },
 })
